e329532d2cd594f49adc05e0d976873fceee4c18
Finity_Cardinal <142924829+devildeuteragonist@users.noreply.github.com>
2024-10-08 03:55:42 +0000

    making very slow progress

--- silly_coding_practice/wordgame/wordgame-code-maybe.py
+++ silly_coding_practice/wordgame/wordgame-code-maybe.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
+
+# importing those 1000 most common words
 f = open('1000-common-words.txt', 'r')
-content = f.read()
-print(content) 
+our_words = f.read() 
+
+# utilising them for our game 
+import random 
+puter_word = random.choice(our_words)
+print(puter_word)
+
+# it reads each individual letter as a separate item in the list
+# yeah i dont want that, but i cant remember how to fix it and i have homework
+# oh well 
