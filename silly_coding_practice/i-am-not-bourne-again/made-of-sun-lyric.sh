cc7f11699de4f920a63f4719f7caad51155d67dd
Finity_Cardinal <142924829+devildeuteragonist@users.noreply.github.com>
2024-10-14 13:00:50 +0000

    renamed my timed music project

---
+++ silly_coding_practice/i-am-not-bourne-again/made-of-sun-lyric.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# i love the sleep command in bash, so why not take advantage of it to create art that is timed perfectly to music? 
+# wondering what song I should do. also what unicode characters i should use, which is also a struggle.  
+sleep 1
+echo "3..."
+sleep 1 
+echo "2..."
+sleep 1
+echo "1..."
+sleep 1
+echo "START!" 
