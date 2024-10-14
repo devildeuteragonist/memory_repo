2d4cab8f05b750d73e8587aff7821520b7dadc21
Finity_Cardinal <142924829+devildeuteragonist@users.noreply.github.com>
2024-10-14 08:02:05 -0400

    Update musically-timed-ascii.sh

--- silly_coding_practice/i-am-not-bourne-again/musically-timed-ascii.sh
+++ silly_coding_practice/i-am-not-bourne-again/musically-timed-ascii.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # i love the sleep command in bash, so why not take advantage of it to create art that is timed perfectly to music? 
-# wondering what song I should do. also what unicode characters i should use, which is also a struggle.  
+# considering doing 'made of sun' by kyau & albert - the album version, beginning at the one minute, eight second mark. 
 sleep 1
 echo "3..."
 sleep 1 
@@ -10,3 +10,30 @@ sleep 1
 echo "1..."
 sleep 1
 echo "START!" 
+
+# more stuff needs to go here
+
+# C H O R U S 
+string1="YOU ARE MADE OF SUN"
+for (( i=0; i<${#string1}; i++ )); do
+    echo -e -n "${string1:$i:1}"
+    sleep 0.1
+done
+
+echo -e '\n'
+sleep 1
+
+string2="TAKE ME TO"
+for (( i=0; i<${#string2}; i++ )); do
+    echo -n "${string2:$i:1}"
+    sleep 0.1
+done
+
+echo -e '\n'
+sleep 0.5
+
+string3="T H E  L I G H T"
+for (( i=0; i<${#string3}; i++ )); do
+    echo -n "${string3:$i:1}"
+    sleep 0.1
+done
