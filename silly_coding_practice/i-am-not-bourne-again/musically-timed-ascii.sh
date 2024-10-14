08cee725da9144370037ad7c3283e02b77183744
Finity_Cardinal <142924829+devildeuteragonist@users.noreply.github.com>
2024-10-14 08:50:51 -0400

    Update musically-timed-ascii.sh

--- silly_coding_practice/i-am-not-bourne-again/musically-timed-ascii.sh
+++ silly_coding_practice/i-am-not-bourne-again/musically-timed-ascii.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# i love the sleep command in bash, so why not take advantage of it to create art that is timed perfectly to music? 
-# considering doing 'made of sun' by kyau & albert - the album version, beginning at the one minute, eight second mark. 
+# i love the sleep command in bash, so why not take advantage of it to time lyrics to music?
+# the following script is for 'made of sun' by kyau and albert, beginning at the 1 minute, 23 second mark.
+# when you execute the script, you should have a countdown. press your play button RIGHT when you see 'START!'
+
 sleep 1
 echo "3..."
 sleep 1 
@@ -11,8 +13,6 @@ echo "1..."
 sleep 1
 echo "START!" 
 
-# more stuff needs to go here
-
 # C H O R U S 
 string1="YOU ARE MADE OF SUN"
 for (( i=0; i<${#string1}; i++ )); do
@@ -21,7 +21,7 @@ for (( i=0; i<${#string1}; i++ )); do
 done
 
 echo -e '\n'
-sleep 1
+sleep 4.5
 
 string2="TAKE ME TO"
 for (( i=0; i<${#string2}; i++ )); do
@@ -30,10 +30,42 @@ for (( i=0; i<${#string2}; i++ )); do
 done
 
 echo -e '\n'
-sleep 0.5
+sleep 2
 
 string3="T H E  L I G H T"
 for (( i=0; i<${#string3}; i++ )); do
     echo -n "${string3:$i:1}"
+    sleep 0.15
+done
+
+echo -e '\n'
+sleep 2
+
+string1="Y O U  A R E  M A D E  O F  S U N"
+for (( i=0; i<${#string1}; i++ )); do
+    echo -n -e "${string1:$i:1}"
+    sleep 0.075
+done
+
+echo -e '\n'
+sleep 4.2
+
+string2="TAKE ME TO"
+for (( i=0; i<${#string2}; i++ )); do
+    echo -n "${string2:$i:1}"
     sleep 0.1
 done
+
+echo -e '\n'
+sleep 2.5
+
+echo "T H E"
+sleep 0.4
+
+string3="L  I  G  H  T"
+for (( i=0; i<${#string3}; i++ )); do
+    echo -n "${string3:$i:1}"
+    sleep 0.033
+done
+
+echo -e '\n'
