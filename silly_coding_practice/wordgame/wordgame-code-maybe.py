00fd1fb0181c7526d4241037256f862da9aea280
Finity_Cardinal <142924829+devildeuteragonist@users.noreply.github.com>
2024-12-21 23:13:47 +0000

    testing to see if it responds to interaction from user

--- silly_coding_practice/wordgame/wordgame-code-maybe.py
+++ silly_coding_practice/wordgame/wordgame-code-maybe.py
@@ -17,3 +17,12 @@ puter_word = random.choice(our_words)
 # computer prompts YOU to guess what it's saying 
 print(f"I'm thinking of a word that is {len(puter_word)} letters long...")
 print(f"...that starts with {puter_word[0]}.")
+guessing = input("Enter your guess: ")
+print(guessing)
+print(puter_word)
+
+# return whether guess was correct or incorrect
+if guessing == puter_word: 
+    print("You guessed it!")
+else: 
+    print("Try again.")
